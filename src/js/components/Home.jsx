bbbaddbacd48b019728812293bc9724adc81d866
JoseDHernandez <168580718+Josedavidh@users.noreply.github.com>
2025-05-07 23:14:06 +0000

    Saved

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,77 @@
-import React from "react";
+import { useState } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const initialStateTask = {
+    task: "",
+    isDone: false
+}
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+    const [task, setTask] = useState(initialStateTask)
+    const [taskList, setTaskList] = useState([])
+
+    const HandleChange = (event) => {
+        setTask({
+            ...task,
+            task: event.target.value
+        })
+    }
+
+    const saveTask = (event) => {
+        if (event.key == "Enter") {
+            setTaskList((prev) => [...prev, task])
+            setTask(initialStateTask)
+        }
+    }
+
+    const deleteTask = (id) => {
+        setTaskList((prev) => prev.filter((_, index) => id != index))
+    }
+
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-12 col-md-7 col-lg-6">
+                    <h1 className="display-1 text-center" style={{ color: "rgb(235, 211, 212)" }}>To Do's</h1>
+                    <div className="border border-bottom-0">
+                        <form
+                            onSubmit={(event) => event.preventDefault()}>
+                            <input
+                                type="text"
+                                className=""
+                                name="task"
+                                placeholder="Enter a the task"
+                                value={task.task}
+                                onChange={HandleChange}
+                                onKeyDown={saveTask}
+                            />
+                        </form>
+
+                        <ul>
+                            {
+                                taskList.map((item, index) => {
+                                    return (
+                                        <li key={index}>
+                                            {item.task}
+                                            <span>
+                                                <i onClick={() => deleteTask(index)}>x</i>
+                                            </span>
+                                        </li>
+                                    )
+                                })
+                            }
+
+                        </ul>
+                        <span className="all-task-info">
+                            {
+                                `${taskList.length} item left`
+                            }
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 export default Home;
---
+++ src/js/components/ToDoList.jsx
@@ -0,0 +1,147 @@
+import { useEffect, useState } from 'react';
+
+const initialStateTask = {
+    label: "",
+    is_done: false
+}
+
+const urlBase = 'https://playground.4geeks.com/todo'
+
+const ToDoList = () => {
+
+    const [task, setTask] = useState(initialStateTask)
+    const [taskList, setTaskList] = useState([])
+
+    const HandleChange = (event) => {
+        setTask({
+            ...task,
+            label: event.target.value
+        })
+    }
+
+    const saveTask = async (event) => {
+
+        if (event.key == "Enter") {
+               try{
+                const response = await fetch(`${urlBase}/todos/jose`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(task)
+                })
+
+                if(response.ok) {
+                    getAllTask()
+                    setTask(initialStateTask)
+                }
+
+               } catch (error) {
+                console.log(error)
+               }
+            }
+    }
+
+    const deleteTask = async (id) => {
+        try {
+            const response = await fetch(`${urlBase}/todos/${id}`, {
+                method: "DELETE",
+            })
+            if (response.ok) {
+                getAllTask()
+            }
+
+        } catch (error) {
+
+        }
+    }
+
+    const deleteAllTasks = async () => {
+        try {
+            const response = await fetch(`${urlBase}/users/jose`, {
+                method: "DELETE"
+            })
+
+            if (response.ok) {
+                setTaskList([])
+            } else {
+                console.log("Error erasing all tasks")
+            }
+
+        } catch (error) {
+            console.log("Error erasing all tasks", error)
+
+        }
+    }
+
+    const getAllTask = async () => {
+        try {
+            const response = await fetch(`${urlBase}/users/jose`)
+            const data = await response.json()
+
+            if(response.ok) {
+                setTaskList(data.todos)
+            }
+            if (response.status == 404){
+
+            }
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getAllTask()
+    }, [])
+
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-12 col-md-7 col-lg-6">
+                    <h1 className="display-1 text-center" style={{ color: "rgb(235, 211, 212)" }}>To Do's</h1>
+                    <div className="border border-bottom-0">
+                        <form
+                            onSubmit={(event) => event.preventDefault()}>
+                            <input
+                                type="text"
+                                className=""
+                                name="label"
+                                placeholder="Enter a the task"
+                                value={task.label}
+                                onChange={HandleChange}
+                                onKeyDown={saveTask}
+                            />
+                        </form>
+
+                        <ul>
+                            {
+                                taskList.map((item, index) => {
+                                    return (
+                                        <li key={item.id}>
+                                            {item.label}
+                                            <span>
+                                                <i onClick={() => deleteTask(item.id)}>x</i>
+                                            </span>
+                                        </li>
+                                    )
+                                })
+                            }
+
+                        </ul>
+                        <span className="all-task-info">
+                            {
+                                `${taskList.length} item left`
+                            }
+                        </span>
+                    </div>
+                </div>
+                <div className="d-flex justify-content-center mt-3">
+                    <button type="button" className="btn btn-primary" onClick={deleteAllTasks}>Erase all tasks</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ToDoList;
